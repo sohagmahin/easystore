b576f29ca6d4dd205ae040a1f6d052122701319a
sohagmahin <sohagmahin@gmail.com>
2025-03-20 13:25:38 +0600

    zod validation and type interface

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,13 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// Convert prisma object into regular object
 export function convertToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
+}
+
+// Format number with decimal places
+export function formatNumberWithDecimal(num: number): string {
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
